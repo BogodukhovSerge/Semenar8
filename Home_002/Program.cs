6001d485f11c428dfc41bfac4b76c7a977522d51
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-21 16:35:31 +0300

    DZ_002 SumStr

--- Home_002/Program.cs
+++ Home_002/Program.cs
@@ -9,3 +9,37 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
 // наименьшей суммой элементов: 1 строка
 
+
+// int[,] array = new int[m,n]
+
+
+int InPutNum(string[,] InPut)
+{
+    Console.Write(InPut);
+    int num = Convert.ToInt32(Consolr.ReadLine());
+    return num;
+}
+
+
+
+void CreatArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i,j] = new Random.Next(renge);
+            }
+
+    }
+}
+
+int SumStrokElement(int[,] array, int i)
+{
+    int SumStrok = array[i,0];
+    for (int j = 0; j < array.GetLength(1); j++)    
+    {
+        SumStrok += array[i,j];
+    }
+    return SumStrok;
+}
